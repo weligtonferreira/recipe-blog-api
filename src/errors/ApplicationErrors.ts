a1865b9170d697f734eceb917a8bb8e6839d1923
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-06-22 18:21:27 -0300

    feat: add a handle exceptions middleware and a class to throw errors

---
+++ src/errors/ApplicationErrors.ts
@@ -0,0 +1,11 @@
+class ApplicationErrors {
+  readonly message: string;
+  readonly statusCode: number;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default ApplicationErrors;
---
+++ src/middlewares/handleExceptions.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+
+import ApplicationErrors from '../errors/ApplicationErrors';
+
+export function handleExceptions(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Response<any, Record<string, any>> {
+  if (err instanceof ApplicationErrors) {
+    return res.status(err.statusCode).json({ error: err.message });
+  }
+
+  return res.status(500).json({
+    status: 'Error',
+    message: 'Server internal error',
+  });
+}
