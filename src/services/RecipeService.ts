991f6582d9f3e8943d03cc3dd893494500bd4074
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-06-27 19:21:07 -0300

    feat: add the implementation of IRecipeService

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,20 +1,43 @@
 import { Recipe } from '@prisma/client';
 
 import IRecipeService from '../dto/recipe/IRecipeService';
+import RecipeRepository from '../repositories/RecipeRepository';
+import ApplicationErrors from '../errors/ApplicationErrors';
 
 class RecipeService implements IRecipeService {
-  async create(createRecipeDataInput: Recipe): Promise<Recipe> {}
+  async create(createRecipeDataInput: Recipe): Promise<Recipe> {
+    const recipe = await RecipeRepository.create(createRecipeDataInput);
 
-  async findAll(): Promise<Recipe[] | null> {}
+    return recipe;
+  }
 
-  async findById(id: string): Promise<Recipe | null> {}
+  async findAll(): Promise<Recipe[] | null> {
+    const recipes = await RecipeRepository.findAll();
 
-  async updateById(
-    id: string,
-    updateRecipeDataInput: Recipe
-  ): Promise<Recipe> {}
+    if (!recipes) throw new ApplicationErrors('Recipes not found!', 404);
 
-  async deleteById(id: string): Promise<void> {}
+    return recipes;
+  }
+
+  async findById(id: string): Promise<Recipe | null> {
+    const recipe = await RecipeRepository.findById(id);
+
+    if (recipe) throw new ApplicationErrors('Recipe not found!', 404);
+
+    return recipe;
+  }
+
+  async updateById(id: string, updateRecipeDataInput: Recipe): Promise<Recipe> {
+    await this.findById(id);
+
+    return await RecipeRepository.updateById(id, updateRecipeDataInput);
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await this.findById(id);
+
+    await RecipeRepository.deleteById(id);
+  }
 }
 
 export default new RecipeService();
