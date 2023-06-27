ab6acb79c1ace2b6bfb54d548a4364c77f3c0f9a
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-06-27 17:54:12 -0300

    feat: add the implementation of IRecipeRepository

--- src/repositories/RecipeRepository.ts
+++ src/repositories/RecipeRepository.ts
@@ -1,20 +1,31 @@
 import { Recipe } from '@prisma/client';
 
 import IRecipeRepository from '../dto/recipe/IRecipeRepository';
+import { prismaClient } from '../../prisma/prismaClient';
 
 class RecipeRepository implements IRecipeRepository {
-  async create(createRecipeDataInput: Recipe): Promise<Recipe> {}
+  async create(createRecipeDataInput: Recipe): Promise<Recipe> {
+    return await prismaClient.recipe.create({ data: createRecipeDataInput });
+  }
 
-  async findAll(): Promise<Recipe[] | null> {}
+  async findAll(): Promise<Recipe[] | null> {
+    return await prismaClient.recipe.findMany();
+  }
 
-  async findById(id: string): Promise<Recipe | null> {}
+  async findById(id: string): Promise<Recipe | null> {
+    return await prismaClient.recipe.findUnique({ where: { id } });
+  }
 
-  async updateById(
-    id: string,
-    updateRecipeDataInput: Recipe
-  ): Promise<Recipe> {}
+  async updateById(id: string, updateRecipeDataInput: Recipe): Promise<Recipe> {
+    return await prismaClient.recipe.update({
+      where: { id },
+      data: updateRecipeDataInput,
+    });
+  }
 
-  async deleteById(id: string): Promise<void> {}
+  async deleteById(id: string): Promise<void> {
+    await prismaClient.recipe.delete({ where: { id } });
+  }
 }
 
 export default new RecipeRepository();
