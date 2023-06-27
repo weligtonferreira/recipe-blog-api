3410b07d8299228444862180aab2add6233dbed3
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-06-27 19:28:43 -0300

    feat: add the implementation of IRecipeController

--- src/controllers/RecipeController.ts
+++ src/controllers/RecipeController.ts
@@ -1,17 +1,47 @@
 import { Request, Response } from 'express';
 
 import IRecipeController from '../dto/recipe/IRecipeController';
+import RecipeService from '../services/RecipeService';
 
 class RecipeController implements IRecipeController {
-  async create(req: Request, res: Response) {}
+  async create(req: Request, res: Response) {
+    const createRecipeDataInput = req.body;
 
-  async findAll(req: Request, res: Response) {}
+    const user = await RecipeService.create(createRecipeDataInput);
 
-  async findById(req: Request, res: Response) {}
+    return res.status(201).json(user);
+  }
 
-  async updateById(req: Request, res: Response) {}
+  async findAll(req: Request, res: Response) {
+    const users = await RecipeService.findAll();
 
-  async deleteById(req: Request, res: Response) {}
+    return res.status(200).json(users);
+  }
+
+  async findById(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const user = await RecipeService.findById(id);
+
+    return res.status(200).json(user);
+  }
+
+  async updateById(req: Request, res: Response) {
+    const { id } = req.params;
+    const updateRecipeDataInput = req.body;
+
+    const user = await RecipeService.updateById(id, updateRecipeDataInput);
+
+    return res.status(200).json(user);
+  }
+
+  async deleteById(req: Request, res: Response) {
+    const { id } = req.params;
+
+    await RecipeService.deleteById(id);
+
+    return res.status(200).json({ message: 'Recipe deleted successfully!' });
+  }
 }
 
 export default new RecipeController();
