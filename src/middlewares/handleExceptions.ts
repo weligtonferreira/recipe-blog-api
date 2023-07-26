a9904e4372a953d87bc7af59155593441cc469f2
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-07-25 21:23:53 -0300

    feat: add response to yup validation errors

--- src/middlewares/handleExceptions.ts
+++ src/middlewares/handleExceptions.ts
@@ -1,6 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 
 import ApplicationErrors from '../errors/ApplicationErrors';
+import { ValidationError } from 'yup';
+
+interface IValidationErrors {
+  [key: string]: string[];
+}
 
 export function handleExceptions(
   err: Error,
@@ -10,6 +15,16 @@ export function handleExceptions(
 ): Response<any, Record<string, any>> {
   if (err instanceof ApplicationErrors) {
     return res.status(err.statusCode).json({ error: err.message });
+  }
+
+  if (err instanceof ValidationError) {
+    const errors: IValidationErrors = {};
+
+    err.inner.forEach((e) => {
+      errors[e.path as string] = e.errors;
+    });
+
+    return res.status(400).json({ message: 'Validation errors:', errors });
   }
 
   console.log(err);
