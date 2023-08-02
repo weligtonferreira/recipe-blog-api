4ee653e0976df230c4ee9e73ef16afb600253c9b
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-08-02 09:43:12 -0300

    feat: create recipe creation validation with yup

---
+++ src/utils/recipeValidation.ts
@@ -0,0 +1,18 @@
+import { Recipe } from '@prisma/client';
+import { object, string, ValidationError } from 'yup';
+
+export async function createRecipeValidation(
+  recipeObject: Recipe
+): Promise<void> {
+  const schema = object().shape({
+    title: string().min(3).required(),
+    description: string().min(3).required(),
+    imagePath: string(),
+  });
+
+  try {
+    await schema.validate(recipeObject, { abortEarly: false });
+  } catch (error) {
+    throw new ValidationError(error as ValidationError);
+  }
+}
